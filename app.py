c5649752ec1da632b3a75590809c45075a377f60
ekayasare <info.emmanuelasare@gmail.com>
2024-10-22 20:38:16 -0700

    changes to streamlit

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
-st.write('It is not a functional application yet. Under cnstruction.')
+st.write('It is not a functional application yet. Under construction.')
